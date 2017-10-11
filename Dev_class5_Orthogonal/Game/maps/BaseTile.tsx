f6daad120b9ab449525da06083c3852c3ac6b955
Farmak09 <farmak9@gmail.com>
2017-10-11 19:43:01 +0200

    maps made with tiled have been added

---
+++ Dev_class5_Orthogonal/Game/maps/BaseTile.tsx
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Base Tileset" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="Metroidvania Tileset.png" width="256" height="256"/>
+ <tile id="0">
+  <objectgroup draworder="index"/>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+</tileset>
--- Dev_class5_Orthogonal/Game/maps/TestTile.tsx
+++ Dev_class5_Orthogonal/Game/maps/TestTile.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="Metroidvania Tileset" tilewidth="32" tileheight="32" tilecount="64" columns="8">
- <image source="../Downloads/Metroidvania Tileset.png" width="256" height="256"/>
+ <image source="Metroidvania Tileset.png" width="256" height="256"/>
  <tile id="0">
   <objectgroup draworder="index"/>
  </tile>
