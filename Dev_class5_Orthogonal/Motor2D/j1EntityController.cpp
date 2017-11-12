89bf2e9ca28b3ff47f8d681ea92b4ef742be8f97
Lorién <lorienportella@gmail.com>
2017-11-12 11:33:26 +0100

    timers and framerate funcionality added
    entities are still being worked in

--- Dev_class5_Orthogonal/Motor2D/j1EntityController.cpp
+++ Dev_class5_Orthogonal/Motor2D/j1EntityController.cpp
@@ -1,45 +1,50 @@
 #include "j1EntityController.h"
+#include "Entity.h"
 
 bool j1EntityController::Update(float dt)
 {
+	bool ret = false;
 	p2List_item<Entity>* tmp = Entities.start;
 	while (tmp != nullptr)
 	{
-		tmp->data.Update(dt);
+		ret = tmp->data.Update(dt);
 		tmp = tmp->next;
 	}
-	return true;
+	return ret;
 }
 
 bool j1EntityController::CleanUp()
 {
+	bool ret = true;
 	p2List_item<Entity>* tmp = Entities.start;
 	while (tmp != nullptr)
 	{
 		tmp->data.CleanUp();
 		tmp = tmp->next;
 	}
-	return true;
+	return ret;
 }
 
 bool j1EntityController::Save()
 {
+	bool ret = true;
 	p2List_item<Entity>* tmp = Entities.start;
 	while (tmp != nullptr)
 	{
 		tmp->data.Save();
 		tmp = tmp->next;
 	}
-	return true;
+	return ret;
 }
 
 bool j1EntityController::Load()
 {
+	bool ret = true;
 	p2List_item<Entity>* tmp = Entities.start;
 	while (tmp != nullptr)
 	{
 		tmp->data.Load();
 		tmp = tmp->next;
 	}
-	return true;
+	return ret;
 }
