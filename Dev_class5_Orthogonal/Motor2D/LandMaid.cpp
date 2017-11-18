f137ab51843295aa980e54200c9c452e7aebb306
Farmak09 <farmak9@gmail.com>
2017-11-18 18:34:10 +0100

    land maid wip

--- Dev_class5_Orthogonal/Motor2D/LandMaid.cpp
+++ Dev_class5_Orthogonal/Motor2D/LandMaid.cpp
@@ -7,7 +7,32 @@ LandMaid::LandMaid() : Enemy(entityType::LAND_ENEMY)
 	canFly = false;
 }
 
+LandMaid::LandMaid(iPoint position) : Enemy(entityType::LAND_ENEMY, position)
+{
+	LoadPushbacks();
+	Current_Animation = &idle;
+	canFly = false;
+}
 
 LandMaid::~LandMaid()
+{
+}
+
+void LandMaid::LoadPushbacks()
+{
+	idle.PushBack({ 732,575,53,53 });
+	idle.PushBack({ 796,575,53,53 });
+	idle.PushBack({ 860,575,53,53 });
+	idle.loop = true;
+	idle.speed = 0.3;
+
+	moving.PushBack({ 732, 633,53,53 });
+	moving.PushBack({ 796, 633,53,53 });
+	moving.loop = true;
+	moving.speed = 0.3;
+}
+
+
+void LandMaid::CleanUp()
 {
 }
--- Dev_class5_Orthogonal/Motor2D/LandMaid.h
+++ Dev_class5_Orthogonal/Motor2D/LandMaid.h
@@ -1,9 +1,18 @@
-#pragma once
 #include "Enemy.h"
+
 class LandMaid : public Enemy
 {
 public:
 	LandMaid();
+	LandMaid(iPoint position);
 	~LandMaid();
+
+private:
+	void LoadPushbacks();
+	void CleanUp();
+
+private:
+	Animation idle;
+	Animation moving;
 };
 
